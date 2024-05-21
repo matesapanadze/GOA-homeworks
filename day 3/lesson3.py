219daa30ec50b315cde9f7d543c37f7593bf3983
matesapanadze <matesapanadze@gmail.com>
2024-05-22 01:53:07 +0400

    second commit

---
+++ day 3/lesson3.py
@@ -0,0 +1,12 @@
+print(2 + 3) #additon
+print(10 - 7) #substraction
+print(15 * 6) #multiplication
+print(30 / 5) #dividion
+
+print("ma" + "te") 
+print("5" + "5")
+#ტერმინალს ვიყენებთ იმისთვის, რომ ჩვენი დაწერილი კოდი გაშიფროს print ფუნქციის მეშვეობით
+name = "mate"
+surname = "sapanadze"
+print(name)
+print(surname)
