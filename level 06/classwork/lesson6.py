f1b83d5ca3aa999d6561af9daaae5dbdc6e1b799
matesapanadze <matesapanadze@gmail.com>
2024-06-01 20:01:51 +0400

    sixth commit

---
+++ level 06/classwork/lesson6.py
@@ -0,0 +1,17 @@
+#input ფუნქციით შეგვიძლია შვიტანოთ ინფორმაცია
+#output ფუნქციით პირიქით, ინფორმაციის გამოტანა შეგვოძლია
+num1 = int(input("please enter number 1: "))
+num2 = int(input("please enter number 2: "))
+num3 = int(input("please enter number 3: "))
+num4 = int(input("please enter number 4: "))
+print(num1 + num2 + num3 + num4)
+print(num1 - num2 - num3 - num4)
+print(num1 * num2 * num3 * num4)
+print(num1 / num2 / num3 / num4)
+
+#registration_simulation
+user_firstname = input("please enter your firstname")
+user_lastname = input("please enter your lastname")
+user_age = input("please enter your age")
+user_email = input("please enter your email")
+print(user_firstname,user_lastname,user_age,user_email)
---
+++ level 06/homework/lesson6.py
@@ -0,0 +1,34 @@
+#about my mother
+name = (input("please enter your mother's name: "))
+surname = (input("please enter your mother's surname: "))
+age = int(input("please enter your mother's age: "))
+gmail = (input("please enter your mother's gmail: "))
+print(name,surname,age,gmail)
+print(age + 20)
+
+#about my grandmother
+name = (input("please enter your grandmother's name: "))
+surname = (input("please enter your grandmother's surname: "))
+age = int(input("please enter your grandmother's age: "))
+gmail = (input)("please enter your grandmother's gmail: ")
+print(name,surname,age,gmail)
+print(age + 20)
+
+#about my brother
+name = (input("please enter your brother's name: "))
+surname = (input("please enter your brother's surname: "))
+age = int(input("please enter your brother's age: "))
+print(name,surname,age,)
+print(age + 20)
+
+#about my sister
+name = (input("please enter your sister's name: "))
+surname = (input("please enter your sister's surname: "))
+age = int(input("please enter your sister's age: "))
+print(name,surname,age,)
+print(age + 20)
+
+
+
+
+
