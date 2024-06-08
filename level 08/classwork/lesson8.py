53ae5594130f0ad0599e96e7fd9403ba52bbe2b1
matesapanadze <matesapanadze@gmail.com>
2024-06-08 22:41:45 +0400

    seventh commit

---
+++ level 08/classwork/lesson8.py
@@ -0,0 +1,10 @@
+user_age = int(input("please enter the age: "))
+print(18 < user_age < 25)
+
+budget = int(input("please enter the budget: "))
+print(budget > 50 or budget > 100)
+
+for i in range(10):
+  print("mate")
+
+
---
+++ level 08/homework/lesson8.py
@@ -0,0 +1,30 @@
+#num1 = int(input("please enter the first number: "))
+#num2 = int(input("please enter the second number: "))
+#num3 = int(input("please enter the third number: "))
+#num4 = int(input("please enter the fourth number: "))
+#num5 = int(input("please enter the fifth number: "))
+
+#print(num1+num2+num3+num4+num5)
+#print(num1-num2-num3-num4-num5)
+#print(num1*num2*num3*num4*num5)
+#print(num1/num2/num3/num4/num5)
+
+
+#age = int(input("please enter the age: "))
+#print(age > 18 and age < 20)
+
+#print(8 > 5 and 10 < 9)
+#print(20 > 19 and 14 < 17)
+#print(40 > 12 or 25 <= 15)
+#print(5 == 5 or 10 >= 9)
+#print(100 == 73 and 12 <= 100)
+
+
+input_1 = int(input())
+input_2 = float(input())
+
+print(input_1 > input_2)
+print(input_1 < input_2)
+print(input_1 >= input_2)
+print(input_1 <= input_2)
+print(input_1 == input_2)
