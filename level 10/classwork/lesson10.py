ea06e6b4361442f34c209b97aeab43f53b6fd5cd
matesapanadze <matesapanadze@gmail.com>
2024-06-13 23:32:01 +0400

    eighth commit

---
+++ level 10/classwork/lesson10.py
@@ -0,0 +1,40 @@
+password = input("please enter the password: ")
+
+
+if password =="luka1234":
+    print("you passed the authorization successfully")
+else:
+    print("the password is incorrect")
+
+sum = 1
+for i in range(5):
+    num = int(input("please enter the number: "))
+    sum = sum * num
+    
+print(sum)
+
+
+sum = 0
+for num in range(5,16):
+    sum = sum + num 
+
+print(sum)
+
+
+
+
+
+
+
+    
+
+
+
+    
+ 
+
+
+    
+
+
+    
---
+++ level 10/homework/lesson10.py
@@ -0,0 +1,32 @@
+sum1 = 0
+sum2 = 0
+sum3 = 1
+
+for num in range(100):
+    sum1 = sum1 + num
+    sum2 = num - sum2
+    sum3 = num * sum3
+     
+print(sum1)
+print(sum2)
+print(sum3)
+
+
+sum = 0
+num = int(input("please enter the number: "))
+for num in range(num):
+    sum = sum + num
+print(sum)
+
+sum_1 = 1
+
+sum1 = 0
+sum2 = 1
+for num in range (2):
+    num = int(input("please enter the number: "))
+    sum1 = sum1 + num
+    sum2 = sum2 * num
+print(sum1)
+print(sum2)
+
+
