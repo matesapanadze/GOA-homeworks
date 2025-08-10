035e56c65584db1fe1e670657168a470f0d45717
matesapanadze <matesapanadze@gmail.com>
2025-08-10 18:02:13 +0400

    'wndwnwi'

--- level 97/classwork/97 class/src/App.jsx
+++ level 97/classwork/97 class/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import EnhancedTable from '../components/ui/table'
 
 export default function App() {
   return (
     <div>
-      
+      <EnhancedTable />
     </div>
   )
 }
--- level 97/classwork/97 class/vite.config.js
+++ level 97/classwork/97 class/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import tailwindcss from '@tailwindcss/vite';
+
