2bdd2dcf197b46f4cc32af65f83dd98f28077b45
matesapanadze <matesapanadze@gmail.com>
2025-08-10 21:35:29 +0400

    wdunc

--- level 97/classwork/97 class/src/App.jsx
+++ level 97/classwork/97 class/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import EnhancedTable from '../components/ui/table'
+import EnhancedTable from '../components/ui/enhanced-table'
 
 export default function App() {
   return (
